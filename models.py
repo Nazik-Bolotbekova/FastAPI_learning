ae9072b5dc749cf14a129887eac65225db523011
Your Name <bolotbekova09@gmail.com>
2025-09-25 00:44:41 +0600

    orders

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from db import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Text
+from sqlalchemy_utils import ChoiceType
 
 class User(Base):
     __tablename__='users'
@@ -10,3 +11,20 @@ class User(Base):
     password: Mapped[str] = mapped_column(Text,nullable=True)
     is_staff: Mapped[bool] = mapped_column(default=False)
     is_active: Mapped[bool] = mapped_column(default=False)
+
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Choice(Base):
+
+    ORDER_STATUSES = (
+        ('PENDING','pending'),
+        ('IN-TRANSIT','in-transit'),
+        ('DELIVERED','delivered'),
+    )
+
+    __tablename__ = 'orders'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+    order_status: Mapped[ChoiceType] = mapped_column(choices=ORDER_STATUSES)
