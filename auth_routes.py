67073c02c004fa110fff9202fa20b3cd220a6d07
Nazik-Bolotbekova <bolotbekova09@gmail.com>
2025-10-05 11:21:45 +0600

    sighup rout

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
+from db import Session, engine
+from schemas import SighUpModel
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
 @router.get('/authenticate')
 async def register():
     return {"message": "Welcome! What is your name?"}
+
+session = Session(bind=engine)
+
+@router.post('/signup')
+async def signup()
