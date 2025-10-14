16d4da74dc6eff39f37ce8867cd60654357f37bd
Nazik-Bolotbekova <bolotbekova09@gmail.com>
2025-10-14 23:52:02 +0600

    learning logs

--- auth_routes.py
+++ auth_routes.py
@@ -56,3 +56,11 @@ logging.info("An INFO")
 logging.warning("A WARNING")
 logging.error("An ERROR")
 logging.critical("A message of CRITICAL severity")
+
+
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
+logging.debug("A DEBUG Message")
+logging.info("An INFO")
+logging.warning("A WARNING")
+logging.error("An ERROR")
+logging.critical("A message of CRITICAL severity")
